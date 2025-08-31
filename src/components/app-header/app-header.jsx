ba1213a4ac8552fea7bbfb39091b634f6a3a6590
Anya Agarenko <a.agarenko@hyperad.tech>
2025-08-31 17:28:08 +0300

    sort impoers, make imports absolute, add stellar-burger logo to head

---
+++ src/components/app-header/app-header.jsx
@@ -0,0 +1,7 @@
+import { Logo } from "@ya.praktikum/react-developer-burger-ui-components";
+
+const AppHeader = () => {
+  return <Logo />;
+};
+
+export default AppHeader;
--- src/components/app/App.test.tsx
+++ src/components/app/App.test.tsx
@@ -1,11 +1,7 @@
-import React from "react";
-import { render, screen } from "@testing-library/react";
-import App from "./App";
+import { render } from "@testing-library/react";
 
-test("renders learn react link", () => {
+import App from "src/components/app/app";
+
+test("ok", () => {
   render(<App />);
-
-  const linkElement = screen.getByText(/learn react/i);
-
-  expect(linkElement).toBeInTheDocument();
 });
