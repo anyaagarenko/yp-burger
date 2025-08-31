13470b6bd32bc7fb84ee2b42d9b9a1846b992f5c
Anya Agarenko <a.agarenko@hyperad.tech>
2025-08-31 19:52:09 +0300

    add nav to header

--- src/components/app-header/app-header.jsx
+++ src/components/app-header/app-header.jsx
@@ -1,7 +1,36 @@
-import { Logo } from "@ya.praktikum/react-developer-burger-ui-components";
+import {
+  BurgerIcon,
+  ListIcon,
+  Logo,
+  ProfileIcon,
+} from "@ya.praktikum/react-developer-burger-ui-components";
 
 const AppHeader = () => {
-  return <Logo />;
+  return (
+    <header>
+      <nav>
+        <div>
+          <>
+            <BurgerIcon type="primary" />
+            <p>Конструктор</p>
+          </>
+          <>
+            <ListIcon type="primary" />
+            <p>Лента заказов</p>
+          </>
+        </div>
+      </nav>
+      <>
+        <Logo />
+      </>
+      <nav>
+        <>
+          <ProfileIcon />
+          <p>Личный кабинет</p>
+        </>
+      </nav>
+    </header>
+  );
 };
 
 export default AppHeader;
